00dfa41e673e436bb6b3a85a4f025f43ecf03ef7
Mariadriazola1986 <mariaadriazola25@gmail.com>
2020-11-04 15:02:05 -0300

    Update traerPartidosDisponibles.php

--- proyectos de FINAL laboratorio 2020/php/traerPartidosDisponibles.php
+++ proyectos de FINAL laboratorio 2020/php/traerPartidosDisponibles.php
@@ -12,7 +12,9 @@
 	{
 		//var_dump($id_user);
 		$conn = getConnection();
-		$sql="select *from partidos,tipos_de_futbol where ID_USUARIO!=:id_usuario AND partidos.TIPO_DE_FUTBOL=tipos_de_futbol.ID_TIPO ORDER BY partidos.FECHA ASC";
+		
+		//$sql="select *from partidos,tipos_de_futbol where ID_USUARIO!=:id_usuario AND partidos.TIPO_DE_FUTBOL=tipos_de_futbol.ID_TIPO AND partidos.CANTIDAD_DE_JUGADORES_ACTUALES<tipos_de_futbol.JUGADORES_MINIMOS_REQUERIDOS AND partidos.FECHA>=CURDATE() ORDER BY partidos.FECHA ASC";
+		$sql='select *from partidos,tipos_de_futbol where ID_USUARIO!=:id_usuario AND partidos.TIPO_DE_FUTBOL=tipos_de_futbol.ID_TIPO AND partidos.CANTIDAD_DE_JUGADORES_ACTUALES<tipos_de_futbol.JUGADORES_MINIMOS_REQUERIDOS AND partidos.FECHA>=CURDATE() AND partidos.HORA>DATE_FORMAT(NOW( ), "%H:%i:%S" ) ORDER BY partidos.FECHA ASC';
 		$resultados=$conn->prepare($sql);
 		$resultados->execute(array(":id_usuario" => $id_user ));
 		$registros=$resultados->fetchAll(PDO::FETCH_OBJ);
