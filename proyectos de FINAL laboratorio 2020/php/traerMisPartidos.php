ab24039d40cf2a54f48ef6ed9f8d4180dd820c78
Mariadriazola1986 <43587602+Mariadriazola1986@users.noreply.github.com>
2020-11-05 01:24:00 -0300

    se arreglo para que traiga los partidos que son superior a la feha y hora actuales

--- proyectos de FINAL laboratorio 2020/php/traerMisPartidos.php
+++ proyectos de FINAL laboratorio 2020/php/traerMisPartidos.php
@@ -12,13 +12,32 @@
 	{
 		//var_dump($id_user);
 		$conn = getConnection();
-		$sql="select *from partidos,tipos_de_futbol where ID_USUARIO=:id_usuario AND partidos.TIPO_DE_FUTBOL=tipos_de_futbol.ID_TIPO ORDER BY partidos.FECHA ASC";
+		$sql="select *from partidos,tipos_de_futbol where ID_USUARIO=:id_usuario AND partidos.TIPO_DE_FUTBOL=tipos_de_futbol.ID_TIPO AND partidos.CANTIDAD_DE_JUGADORES_ACTUALES<tipos_de_futbol.JUGADORES_MINIMOS_REQUERIDOS AND partidos.FECHA>=CURDATE() ORDER BY partidos.FECHA ASC";
 		$resultados=$conn->prepare($sql);
 		$resultados->execute(array(":id_usuario" => $id_user ));
-		$registros=$resultados->fetchAll(PDO::FETCH_OBJ);
+		$registros=$resultados->fetchAll(PDO::FETCH_ASSOC);
 		closeConnection($conn);
-		echo json_encode($registros);
-		
+		$hora_actual = new DateTime("now", new DateTimeZone('America/Argentina/Buenos_Aires'));
+
+		$hora_now=$hora_actual->format('H:i:s');
+		$Hoy = new DateTime("now", new DateTimeZone('America/Argentina/Buenos_Aires'));
+		$registros_coincidentes=array();
+		foreach ($registros as $hora_del_partido) {
+			if ($hora_del_partido["FECHA"]==$Hoy->format('Y-m-d')) {
+
+				if ($hora_del_partido["HORA"]>$hora_now) {
+
+					array_push($registros_coincidentes, $hora_del_partido);
+				}
+			}
+			else if ($hora_del_partido["FECHA"]>$Hoy->format('Y-m-d')) {
+					array_push($registros_coincidentes, $hora_del_partido);
+			}
+
+
+		}
+		echo json_encode($registros_coincidentes);
+
 	}
 
 
