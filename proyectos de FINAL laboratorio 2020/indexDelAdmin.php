106d38e4ea18b71fe81be2911ec875000925f0d3
leanbarrios2018 <71737728+leanbarrios2018@users.noreply.github.com>
2020-11-14 20:53:20 -0300

    Update indexDelAdmin.php

--- proyectos de FINAL laboratorio 2020/indexDelAdmin.php
+++ proyectos de FINAL laboratorio 2020/indexDelAdmin.php
@@ -4,7 +4,7 @@
   		require_once("php/verificarSesion.php");
  	?>
 <head>
-	<title>Mis Grupos</title>
+	<title>Administracion</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
