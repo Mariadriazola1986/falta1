24623c56b410d343d0021ba7cbd30d63e386ef0c
leanbarrios2018 <71737728+leanbarrios2018@users.noreply.github.com>
2020-11-16 19:15:39 -0300

    Resubiendo archivos porque casi la cago (no tengo conociemiento del merge)
    Viendo como darle funcionalidad a los botones generados dentro del AJAX

--- proyectos de FINAL laboratorio 2020/js/mostrarUsuarios.js
+++ proyectos de FINAL laboratorio 2020/js/mostrarUsuarios.js
@@ -2,29 +2,31 @@ $(document).ready(function(){
     obtenerUsuariosRegistrados();
 });
 
-function obtenerUsuariosRegistrados(){
-    $.ajax
-    ({
-        url: "php/traerUsuariosRegistrados.php",
-        type: "POST",
-        dataType: "json",
-        success: function (response) {
-                $.each(response, function(){
-                    $("#tablaUsuariosRegistrados").append($("<tr></tr>").append(
-                        $("<td></td>").text(this.NOMBRE),
-                        $("<td></td>").text(this.EMAIL),
-                        $("<td></td>").text(this.NOMBRE_TIPO),
-                        $("<td></td>").text(this.ESTADO),
-                        $("<td></td>").append(
-                            $("<button></button>").attr("id", this.ID_USUARIO).attr("class", 'btn btn-info').text("Dar de alta al usuario"),
-                            $("<button></button>").attr("id", this.ID_USUARIO).attr("class", 'btn btn-info').attr("data-target", '#modalRegistro').text("Modificar usuario"),
-                            $("<button></button>").attr("id", this.ID_USUARIO).attr("class", 'btn btn-info').text("Dar de baja al usuario"))
-                        ));
-                })
-        },
-        error: function(xhr, status, error){
-                console.log(error);
-                console.log("Pincho y no se por qué :v");
-        }
-    });
-}
+    function obtenerUsuariosRegistrados(){
+        $.ajax
+        ({
+            url: "php/traerUsuariosRegistrados.php",
+            type: "POST",
+            dataType: "json",
+            success: function (response) {
+                    $.each(response, function(){
+                        $("#tablaUsuariosRegistrados").append($("<tr></tr>").append(
+                            $("<td></td>").text(this.NOMBRE),
+                            $("<td></td>").text(this.EMAIL),
+                            $("<td></td>").text(this.NOMBRE_TIPO),
+                            $("<td></td>").text(this.ESTADO),
+                            $("<td></td>").append(
+                                $("<button></button>").attr("id", this.ID_USUARIO).attr("class", 'btn btn-info').text("Dar de alta usuario"),
+                                $("<button></button>").attr("id", this.ID_USUARIO).attr("class", 'btn btn-info').attr("data-target", '#modalRegistro').text("Modificar usuario").on("click", function(){
+                                    console.log("Entro en el boton");
+                                }),
+                                $("<button></button>").attr("id", this.ID_USUARIO).attr("class", 'btn btn-info').text("Dar de baja usuario"))
+                            ));
+                    })
+            },
+            error: function(xhr, status, error){
+                    console.log(error);
+                    console.log("Pincho y no se por qué :v");
+            }
+        });
+    }
