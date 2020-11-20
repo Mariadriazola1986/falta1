a9910c7bface52b8ecdef44d9d93541b624a2227
leanbarrios2018 <leanbarrios2018@gmail.com>
2020-11-20 20:51:17 -0300

    Probando el filtro para traer usuarios que no sea el admin y se agregaron pequeños cambios en la BD

--- proyectos de FINAL laboratorio 2020/php/traerUsuariosRegistrados.php
+++ proyectos de FINAL laboratorio 2020/php/traerUsuariosRegistrados.php
@@ -4,7 +4,7 @@
 
 		//var_dump($id_user);
 		$conn = getConnection();
-		$sql="select usuarios.ID_USUARIO, usuarios.NOMBRE, usuarios.EMAIL, roles.NOMBRE_TIPO, estados_usuarios.ESTADO from usuarios inner join roles on usuarios.TIPO_USUARIO = roles.ID_TIPO inner join estados_usuarios on usuarios.ESTADO_USUARIO = estados_usuarios.ID_ESTADO";
+		$sql="select usuarios.ID_USUARIO, usuarios.NOMBRE, usuarios.EMAIL, roles.NOMBRE_TIPO, estados_usuarios.ESTADO from usuarios inner join roles on usuarios.TIPO_USUARIO = roles.ID_TIPO inner join estados_usuarios on usuarios.ESTADO_USUARIO = estados_usuarios.ID_ESTADO where roles.NOMBRE_TIPO != 'Administrador' ";
 		$resultados=$conn->prepare($sql);
 		$resultados->execute();
 		$registros=$resultados->fetchAll(PDO::FETCH_ASSOC);
